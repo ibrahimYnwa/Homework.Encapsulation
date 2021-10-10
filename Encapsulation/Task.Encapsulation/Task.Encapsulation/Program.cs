8017af7173cbf70ac02dde61add5045593f7fc20
ibrahim <ibrahimea@code.edu.az>
2021-10-10 19:12:15 +0400

    Encapsulation

---
+++ Encapsulation/Task.Encapsulation/Task.Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task.Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon();
+            weapon.maxNumbers = 30;
+            Console.WriteLine(weapon.curNumber);
+            weapon.Shoot();
+            Console.WriteLine(weapon.curNumber);
+            weapon.Reload();
+            Console.WriteLine(weapon.curNumber);
+
+        }
+    }
+}
---
+++ Encapsulation/Task.Encapsulation/Task.Encapsulation/Weapon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task.Encapsulation
+{
+    class Weapon
+    {
+        private int MaxNumbers;
+        private int CurNumber = 30;
+
+        public int curNumber 
+        { 
+            get { return CurNumber; }  
+        }
+        public int maxNumbers
+        {
+            get { return MaxNumbers; }
+            set
+            {
+                if (value < 30) 
+                {
+                    MaxNumbers = 30;
+                }
+                else
+                {
+                    MaxNumbers = value;
+
+                }
+            }
+        }
+        public void Shoot()
+        {
+            if (CurNumber>0)
+            {
+                CurNumber -= 1;
+
+            }
+        }
+        public void Reload()
+        {
+            if (CurNumber<MaxNumbers)
+            {
+                CurNumber = MaxNumbers;
+
+            }
+        }
+    }
+}
